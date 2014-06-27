8d201f5d4240d0494845e8954baa193cf8054a1f
Liomka <tankizer@ymail.com>
2014-06-27 22:31:49 +0200

    Switch for Tornado

--- wsgi.py
+++ wsgi.py
@@ -1,35 +1,31 @@
 import Settings
 
-import cyclone.web
-import cyclone.wsgi
+import tornado.web
+import tornado.wsgi
 import wsgiref.simple_server
-import sys
-
-from twisted.python import log
 
 
-class Application(cyclone.web.Application):
+class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r'/', MainHandler),
             (r'/api', ApiHandler),
-            (r'/favicon.ico', cyclone.web.StaticFileHandler, {'path': "./"}),
+            (r'/favicon.ico', tornado.web.StaticFileHandler, {'path': "./"}),
             ]
         settings = {
             "WSServerUrl": "ws://pywsserver.herokuapp.com/ws",
             "template_path": Settings.TEMPLATE_PATH,
             "static_path": Settings.STATIC_PATH,
             }
-        cyclone.web.Application.__init__(self, handlers, **settings)
+        tornado.web.Application.__init__(self, handlers, **settings)
 
 
-class MainHandler(cyclone.web.RequestHandler):
+class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
 
-class ApiHandler(cyclone.web.RequestHandler):
-    @cyclone.web.asynchronous
+class ApiHandler(tornado.web.RequestHandler):
     def get(self, *args):
         pass
         """
@@ -40,14 +36,11 @@ class ApiHandler(cyclone.web.RequestHandler):
         data = json.dumps(data)
         """
 
-    @cyclone.web.asynchronous
     def post(self):
         pass
 
 if __name__ == "__main__":
     application = Application()
-    log.startLogging(sys.stdout)
-
-    wsgi_app = cyclone.wsgi.WSGIAdapter(application)
-    server = wsgiref.simple_server.make_server('', Settings.HTTPPORT, wsgi_app)
+    wsgiApp = tornado.wsgi.WSGIAdapter(application)
+    server = wsgiref.simple_server.make_server('', Settings.HTTPPORT, wsgiApp)
     server.serve_forever()
