c46e776e07c8dc54e15b48092504f254b1e3fab6
Liomka <tankizer@ymail.com>
2014-06-27 22:48:28 +0200

    Test deploy on port 80 by default

--- Settings.py
+++ Settings.py
@@ -1,6 +1,6 @@
 import os
 dirname = os.path.dirname(__file__)
 
-HTTPPORT = int(os.environ.get("PORT", 5000))
+HTTPPORT = int(os.environ.get("PORT", 80))
 STATIC_PATH = os.path.join(dirname, 'static')
 TEMPLATE_PATH = os.path.join(dirname, 'templates')
