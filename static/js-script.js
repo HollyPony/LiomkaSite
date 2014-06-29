07865ae98d5541c430e598d8ad582cf7a52f31ed
Liomka <arthur.agombart@gmail.com>
2014-06-30 00:29:52 +0200

    Reorganize code.

--- static/js-script.js
+++ static/js-script.js
@@ -22,6 +22,21 @@ window.addEventListener("load", function(event) {
     var sendTextButton = $('#sendText');
 
     var intervalID = null;
+    var socket = null;
+
+    // ------------------------------------------------------------------------
+    // Functions for dom
+    // ------------------------------------------------------------------------
+    function initComponents() {
+        chatLog.prop('disabled', false);
+        userList.prop('disabled', false);
+        userName.prop('disabled', false);
+
+        userInput.prop('disabled', true);
+        sendTextButton.prop('disabled', true);
+
+        toggleConnectionButton.text('Connect');
+    }
 
     function connected() {
         intervalID = setInterval(function(){ping();}, 40000);
@@ -34,7 +49,7 @@ window.addEventListener("load", function(event) {
 
         toggleConnectionButton.text('Disconnect');
 
-        messageEvent('Connected');
+        messageEvent({content: "Connected"});
     }
 
     function connecting() {
@@ -48,32 +63,105 @@ window.addEventListener("load", function(event) {
     }
 
     function disconnected() {
+        initComponents();
         clearInterval(intervalID);
-        chatLog.prop('disabled', false);
-        userList.prop('disabled', false);
-        userName.prop('disabled', false);
-
-        userInput.prop('disabled', true);
-        sendTextButton.prop('disabled', true);
-
-        toggleConnectionButton.text('Connect');
-
-        messageEvent('Disconnected');
+        messageEvent({content: 'Disconnected'});
     }
 
+    function messageWarning(msg) {
+        appendMessage($(document.createTextNode(msg.content)), 'text-warning', msg.time);
+    }
+
+    function messageEvent(msg) {
+        appendMessage($(document.createTextNode(msg.content)), 'text-muted', msg.time);
+    }
+
+    function messageError(msg) {
+        appendMessage($(document.createTextNode(msg.content)), 'text-danger', msg.time);
+    }
+
+    function messageReceived(msg) {
+        // Create the message element
+        appendMessage($(document.createElement('span')).append(
+            $(document.createElement('span')).addClass('userName').addClass('text-info').text(msg.from.name)).append(
+            $(document.createTextNode(msg.content))
+        ), 'messageReceived', msg.time);
+    }
+
+    function appendMessage(blocToAppend, messageType, time) {
+        // Format the date
+        // multiplied by 1000 so that the argument is in milliseconds, not seconds
+        var date = null;
+        if (time) {
+            date = new Date(time * 1000);
+        } else {
+            date = new Date();
+        }
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var seconds = date.getSeconds();
+
+        var formattedTime = hours + ':' + minutes + ':' + seconds;
+
+        // Create the message element
+        chatLog.append($(document.createElement('p')).addClass(messageType).append(
+            $(document.createElement('span')).addClass('timeMessage').addClass('text-muted').text(formattedTime)).append(
+            blocToAppend));
+        chatLog.animate({scrollTop: chatLog[0].scrollHeight}, 'slow');
+    }
+
+    // ------------------------------------------------------------------------
+    // Treat events from WebSockets
+    // ------------------------------------------------------------------------
+    function userListReceived(users) {
+        users.forEach(function (user) {
+            userList.append($(document.createElement('dt')).text(user.name).attr('id', user.id));
+        });
+    }
+
+    function userConnected(user) {
+        userList.append($(document.createElement('dt')).text(user.name).attr('id', user.id));
+    }
+
+    function userDiconnected(user) {
+        var element = $('#'+user.id);
+        if (element) {
+            element.remove();
+        }
+    }
+
+    // ------------------------------------------------------------------------
+    // Send actions to WebSockets
+    // ------------------------------------------------------------------------
+    function ping() {
+        socket.send(JSON.stringify("ping"));
+    }
+
+    function send() {
+        var text = userInput.val();
+
+        if (text != '') {
+            try {
+                socket.send(JSON.stringify({"message": {"content": text}}));
+            } catch (exception) {
+                messageError({content: 'Fail to send a message ' + text});
+            }
+        }
+
+        userInput.val("");
+    }
+
+    // Connect objects events
     if (!("WebSocket" in window)) {
         $('#chatLog, input, button').fadeOut("slow");
         $('<p>Oh no, you need a browser that supports WebSockets. How about <a href="http://www.google.com/chrome">Google Chrome</a>?</p>').appendTo('#container');
     } else {
-
-        var socket;
-
-        disconnected();
+        initComponents();
 
         toggleConnectionButton.click( function (event) {
 
             if (socket) {
-                socket.close();
+                socket.close(1000);
                 return;
             }
 
@@ -106,36 +194,30 @@ window.addEventListener("load", function(event) {
                 socket.onmessage = function (event) {
                     try {
                         var msgObj = JSON.parse(event.data);
-                        if (msgObj == "accepted") {
-                            connected();
-                        } else if (msgObj == "rejected") {
+                        if (msgObj.accepted) {
+                            connected(msgObj.accepted.time);
+                        } else if (msgObj.rejected) {
                             socket.close();
                         } else if (msgObj.message) {
                             messageReceived(msgObj.message);
                         } else if (msgObj.userList) {
-                            msgObj.userList.forEach(function (user) {
-                                userList.append($(document.createElement('dt')).text(user.name).attr('id', user.id));
-                            });
+                            userListReceived(msgObj.userList);
                         } else if (msgObj.userConnected) {
-                            var user = msgObj.userConnected;
-
-                            userList.append($(document.createElement('dt')).text(user.name).attr('id', user.id));
+                            userConnected(msgObj.userConnected);
                         } else if (msgObj.userDisconnected) {
-                            var element = document.getElementById(msgObj.userDisconnected.id);
-                            element.parentNode.removeChild(element);
+                            userDiconnected(msgObj.userDisconnected);
                         } else if (msgObj.userChangeName) {
                             var element = document.getElementById(msgObj.userChangeName.id);
                             element.textContent = msgObj.userChangeName.name;
                         }
                     } catch (e) {
-                        messageWarning(event.data);
+                        messageWarning({content: 'onMessage error: ' + event.data});
                         console.log(e)
                     }
                 };
 
             } catch (exception) {
-                console.log('Error: ' + exception);
-                console.log('Serveur: ' + remoteServer.val())
+                messageWarning({content: "Error: " + exception});
                 disconnected();
                 socket = null;
             }
@@ -151,57 +233,4 @@ window.addEventListener("load", function(event) {
             }
         });
     }
-
-    function ping() {
-        socket.send(JSON.stringify("ping"));
-    }
-
-    function send() {
-        var text = userInput.val();
-
-        if (text != '') {
-            try {
-                socket.send(JSON.stringify({"message": {"content": text}}));
-            } catch (exception) {
-                messageWarning('');
-            }
-        }
-
-        userInput.val("");
-    }
-
-    function messageWarning(msg) {
-        appendMessage($(document.createElement('p')).text(msg).addClass("text-warning"));
-    }
-
-    function messageEvent(msg) {
-        appendMessage($(document.createElement('p')).text(msg).addClass("text-muted"));
-    }
-
-    function messageError(msg) {
-        appendMessage($(document.createElement('p')).text(msg).addClass("text-danger"));
-    }
-
-    function messageReceived(msg) {
-        // Format the date
-        // multiplied by 1000 so that the argument is in milliseconds, not seconds
-        var date = new Date(msg.time*1000);
-        var hours = date.getHours();
-        var minutes = date.getMinutes();
-        var seconds = date.getSeconds();
-
-        var formattedTime = hours + ':' + minutes + ':' + seconds;
-
-        // Create the message element
-        appendMessage($(document.createElement('p')).addClass('messageReceived').append(
-            $(document.createElement('span')).addClass('timeMessage').addClass('text-muted').text(formattedTime)).append(
-            $(document.createElement('span')).addClass('userName').addClass('text-info').text(msg.from.name)).append(
-            $(document.createTextNode(msg.content))
-        ));
-    }
-
-    function appendMessage(blocToAppend) {
-        chatLog.append(blocToAppend);
-        chatLog.animate({scrollTop: chatLog[0].scrollHeight}, 'slow');
-    }
-})
+});
