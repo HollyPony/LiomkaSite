c62ae2807cd2231764c7e9853beae2f4a7917c09
Liomka <arthur.agombart@gmail.com>
2014-07-04 21:16:50 +0200

    Update global theme Using jquery.stellar plugin from Mark Dalgleish

--- static/js/global.js
+++ static/js/global.js
@@ -3,3 +3,13 @@
 /**
  * Created by liomka on 03/07/2014.
  */
+
+$(function () {
+    $.stellar({
+        responsive: true,
+        horizontalScrolling: false,
+        verticalOffset: 0,
+        horizontalOffset: 0,
+        positionProperty: 'position'
+    });
+});
