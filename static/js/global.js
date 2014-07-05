ac549b79f1989a004efbc6f612e4be8fb4f35d79
Liomka <arthur.agombart@gmail.com>
2014-07-05 16:21:13 +0200

    Add animated clouds with CSS3 and Stellar lib

--- static/js/global.js
+++ static/js/global.js
@@ -4,14 +4,43 @@
  * Created by liomka on 03/07/2014.
  */
 
-$(function () {
-    /*
+$(document).ready(function () {
+
+    var sky = $('#sky');
+    var city = $('#city');
+
+    city.css({display: 'block', top: $(document).height() - city.height()});
+    sky.css({display: 'block', top: $('.navbar').height(), height: city.position().top});
+
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        return;
+    }
+
+    function rand(max, min) {
+        min = typeof min !== 'undefined' ? min: 0;
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    var body = $('body');
+    if (city.position().top > 0) {
+        for (var i = 0; i < Math.floor((Math.random() * 23) + 4); ++i) {
+            var chancetoHaveOne = Math.random() * (1.1 - 0.55) + 0.55;
+            chancetoHaveOne = ( chancetoHaveOne > 0.999) ? 0.999  : chancetoHaveOne;
+            city.prepend($(document.createElement('div')).addClass('cloud')
+                    .css({animationDuration: rand(30, 10) + 's', zIndex: Math.floor(chancetoHaveOne.toPrecision(3) * 100)})
+                    .attr({'data-stellar-ratio': chancetoHaveOne})
+                    .delay(rand(10000)).queue(function (next) {
+                        $(this).css({animationPlayState: 'running'});
+                        next();
+                    })
+            );
+        }
+    }
+
     $.stellar({
-        responsive: true,
+        /*responsive: true,*/
         horizontalScrolling: false,
-        verticalOffset: 0,
-        horizontalOffset: 0,
-        positionProperty: 'position'
+        verticalOffset: city.position().top,
+        /*positionProperty: 'position'*/
     });
-    */
 });
