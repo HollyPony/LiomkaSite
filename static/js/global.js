73b6446d869b22185cb0c8e59d6b55264f5c8ca0
Liomka <arthur.agombart@gmail.com>
2014-07-07 00:02:23 +0200

    Update clouds animation (still disabled anyway)

--- static/js/global.js
+++ static/js/global.js
@@ -32,29 +32,29 @@ $(document).ready(function () {
     var city = $('#city');
 
     /*
+    var nbClouds = rand(10, 4);
+    for (var i = 0; i < nbClouds; ++i) {
+        var animationDuration, chanceToHaveOne, zIndex, delay;
 
-    if (city.position().top > 0) {
-        for (var i = 0; i < Math.floor((Math.random() * 23) + 4); ++i) {
-            var animationDuration, chancetoHaveOne, zIndex, delay;
+        animationDuration = rand(100, 50) + 's';
+        chanceToHaveOne = Math.random() * (9.9 - 0.1) + 0.1;
+        zIndex = Math.floor(chanceToHaveOne.toPrecision(3) * 10);
+        delay = rand(15000);
 
-            animationDuration = rand(100, 50) + 's';
-            chancetoHaveOne = Math.random() * (1.1 - 0.55) + 0.55;
-            chancetoHaveOne = ( chancetoHaveOne > 0.999) ? 0.999  : chancetoHaveOne;
-            zIndex = Math.floor(chancetoHaveOne.toPrecision(3) * 100);
-            delay = rand(15000);
-
-            city.prepend($(document.createElement('div')).addClass('cloud')
-                    .css({animationDuration: animationDuration, zIndex: zIndex, display: 'none', opacity: 0, backgroundPosition: '40% initial'})
-                    .attr({'data-stellar-ratio': chancetoHaveOne})
-                    .delay(delay).queue(function (next) {
-                        $(this).css({animationPlayState: 'running'});
-                        $(this).css({animationPlayState: 'running', display: 'block'});
-                        $(this).animate({opacity: 1}, 1000);
-                        next();
-                    })
-            );
-        }
-    }*/
+        city.append($(document.createElement('div'))
+                .addClass('cloud')
+                .css({animationDuration: animationDuration
+                    , zIndex: zIndex
+                    , opacity: 0})
+                .attr({'data-stellar-ratio': chanceToHaveOne
+                    , 'data-sellar-offset-vertical': 400})
+                .delay(delay).queue(function (next) {
+                    $(this).animate({opacity: 1}, 1000);
+                    next();
+                })
+        );
+    }
+    */
 
     $.stellar({
         responsive: true,
