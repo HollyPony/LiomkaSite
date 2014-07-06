bfca6c6eb41e00f04f2672d43f1c4c9088872a46
Liomka <arthur.agombart@gmail.com>
2014-07-06 13:19:50 +0200

    Use scroll spy for affix

--- static/js/index.js
+++ static/js/index.js
@@ -6,27 +6,6 @@
 
 
 $(document).ready(function () {
-    var projects = $('.project');
-    var navigator = $('#navigator');
-
+    var navigator = $('#scrollspy');
     navigator.css({width: navigator.width()});
-
-    //entriesDetails.css({ opacity: '0'});
-
-    projects.waypoint(function(direction) {
-
-        var project = $(this).attr('id');
-        //console.log(project + ' -- ' + direction);
-        var selected = navigator.find("a[href='#"+ project +"']").parent();
-
-        navigator.find('.active').removeClass('active');
-
-        selected.addClass('active');
-        if (selected.parent().parent().parent().is('ul'))
-            selected.parent().parent().parent().addClass('active');
-    }, {
-        //offset: function() {
-        //    return -$(this).height();
-        //}
-    });
 });
