2c4f14dc56e07ca29581272c6f8dfebea56d5789
Liomka <arthur.agombart@gmail.com>
2014-07-06 04:45:22 +0200

    Fix the animation support

--- static/js/global.js
+++ static/js/global.js
@@ -4,6 +4,27 @@
  * Created by liomka on 03/07/2014.
  */
 
+$.support.transition = (function(){
+    var thisBody = document.body || document.documentElement,
+        thisStyle = thisBody.style;
+    return thisStyle.transition !== undefined
+        || thisStyle.WebkitTransition !== undefined
+        || thisStyle.MozTransition !== undefined
+        || thisStyle.msTransition !== undefined
+        || thisStyle.OTransition !== undefined;
+})();
+
+$.support.animation = (function(){
+    var thisBody = document.body || document.documentElement,
+        thisStyle = thisBody.style;
+    return thisStyle.animation !== undefined
+        || thisStyle.WebkitAnimation !== undefined
+        || thisStyle.MozAnimation !== undefined
+        || thisStyle.msAnimation !== undefined
+        || thisStyle.KhtmlAnimation !== undefined
+        || thisStyle.OAnimation !== undefined;
+})();
+
 $(document).ready(function () {
 
     var sky = $('#sky');
@@ -15,30 +36,9 @@ $(document).ready(function () {
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )
         return;
 
-    var animation = false; // --------- If animation is supported
-    /*
-    var    animationstring = 'animation',
-    var    keyframeprefix = '',
-    var    domPrefixes = 'Webkit Moz O ms Khtml'.split(' '),
-    var    pfx  = '';
-    */
-    if( elm.style.animationName !== undefined ) { animation = true; }
-    /*
-    if( animation === false ) {
-        for (var i = 0; i < domPrefixes.length; i++) {
-            if (elm.style[ domPrefixes[i] + 'AnimationName' ] !== undefined) {
-                pfx = domPrefixes[ i ];
-                animationstring = pfx + 'Animation';
-                keyframeprefix = '-' + pfx.toLowerCase() + '-';
-                animation = true;
-                break;
-            }
-        }
-    }
-    */
-
-    if (!animation)
+    if ($.support.animation === undefined) {
         return;
+    }
 
     function rand(max, min) {
         min = typeof min !== 'undefined' ? min: 0;
@@ -48,12 +48,18 @@ $(document).ready(function () {
     var body = $('body');
     if (city.position().top > 0) {
         for (var i = 0; i < Math.floor((Math.random() * 23) + 4); ++i) {
-            var chancetoHaveOne = Math.random() * (1.1 - 0.55) + 0.55;
+            var animationDuration, chancetoHaveOne, zIndex, delay;
+
+            animationDuration = rand(100, 50) + 's';
+            chancetoHaveOne = Math.random() * (1.1 - 0.55) + 0.55;
             chancetoHaveOne = ( chancetoHaveOne > 0.999) ? 0.999  : chancetoHaveOne;
+            zIndex = Math.floor(chancetoHaveOne.toPrecision(3) * 100);
+            delay = rand(15000);
+
             city.prepend($(document.createElement('div')).addClass('cloud')
-                    .css({animationDuration: rand(30, 10) + 's', zIndex: Math.floor(chancetoHaveOne.toPrecision(3) * 100)})
+                    .css({animationDuration: animationDuration, zIndex: zIndex})
                     .attr({'data-stellar-ratio': chancetoHaveOne})
-                    .delay(rand(10000)).queue(function (next) {
+                    .delay(delay).queue(function (next) {
                         $(this).css({animationPlayState: 'running'});
                         next();
                     })
