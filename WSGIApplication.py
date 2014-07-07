f026888eabdf84c8cd4668b6a9b328086ebd5153
Liomka <arthur.agombart@gmail.com>
2014-07-08 00:43:43 +0200

    Add basic apple-touch-icon

--- WSGIApplication.py
+++ WSGIApplication.py
@@ -9,30 +9,33 @@ import tornado.httpserver
 
 class Application(tornado.wsgi.WSGIApplication):
     def __init__(self):
-        handlers = [
-            (r'/', IndexHandler),
-            (r'/cv', CvHandler),
-            (r'/demo', JsWSClientHandler),
-            # (r"/(apple-touch-icon\.png)", tornado.web.StaticFileHandler, dict(path=settings['static_path'])),
-            (r'/favicon.ico', tornado.web.StaticFileHandler, {'path': ""}), # http://www.vectortown.com/
-            ]
         settings = {
             "site_title": "Liomka.IO",
             "template_path": Settings.TEMPLATE_PATH,
             "static_path": Settings.STATIC_PATH,
 
-            #"ui_modules": {"Entry": EntryModule},
-            #"xsrf_cookies": True,
+            # "ui_modules": {"Entry": EntryModule},
+            # "xsrf_cookies": True,
         }
+        handlers = [
+            (r'/', IndexHandler),
+            (r'/cv', CvHandler),
+            (r'/demo', JsWSClientHandler),
+            (r"/(apple-touch-icon\.png)", tornado.web.StaticFileHandler, dict(path=settings['static_path'])),
+            (r'/favicon.ico', tornado.web.StaticFileHandler, {'path': ""})  # http://www.vectortown.com/
+        ]
         tornado.web.Application.__init__(self, handlers, **settings)
+
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
+
 class JsWSClientHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("jswsclient.html", wsserver = Settings.WSSERVER_URI)
+        self.render("jswsclient.html", wsserver=Settings.WSSERVER_URI)
+
 
 class CvHandler(tornado.web.RequestHandler):
     def get(self):
